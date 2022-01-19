a48e4e14e87a80989cf3a3d752d8f6e0fd5a831b
Ivan Berov <ivanberov3@gmail.com>
2022-01-19 14:32:16 +0100

    adds

---
+++ CSharpWebBasics/SoftUniHttpServer/SoftUniHttpServer/Responses/TextFileResponse.cs
@@ -0,0 +1,32 @@
+﻿using SoftUniHttpServer.HTTP;
+
+namespace SoftUniHttpServer.Responses
+{
+    public class TextFileResponse : Response
+    {
+        public TextFileResponse(string fileName)
+            : base(StatusCode.OK)
+        {
+            FileName = fileName;
+
+            this.Headers.Add(Header.ContentType, ContentType.PlainText);
+        }
+
+        public string FileName { get; init; }
+
+        // Should set the correct headers and the body, if a file with the given name exists.
+        public override string ToString()
+        {
+            if (File.Exists(FileName))
+            {
+                this.Body = File.ReadAllTextAsync(FileName).Result;
+
+                var fileBytesCount = new FileInfo(FileName).Length;
+
+                this.Headers.Add(Header.ContentLength, fileBytesCount.ToString());
+                this.Headers.Add(Header.ContentDisposition, $"attachment; filename\"{this.FileName}\"");
+            }
+            return base.ToString();
+        }
+    }
+}
--- CSharpWebBasics/SoftUniHttpServer/SooftUniHttpDemo/StartUp.cs
+++ CSharpWebBasics/SoftUniHttpServer/SooftUniHttpDemo/StartUp.cs
@@ -10,13 +10,56 @@ public class StartUp
                <input type='submit' value ='Save' />
             </form>";
 
-    public static void Main(string[] args)
-        => new HttpServer(routes => routes
+    private const string DownloadForm = @"<form action='/Content' method='POST'>
+                <input type='submit' value ='Download Sites Content' /> 
+            </form>";
+
+    private const string FileName = "content.txt";
+
+    public static async Task Main(string[] args)
+    {
+        await DownloadSitesAsTextFile
+            (StartUp.FileName, new string[] { "https://judge.softuni.org/", "https://softuni.bg/" });
+
+        var server = new HttpServer(routes => routes
         .MapGet("/", new TextResponse("Hello from the server."))
         .MapGet("/Redirect", new RedirectResponse("https://softuni.bg"))
         .MapGet("/HTML", new HtmlResponse(StartUp.HtmlForm))
-        .MapPost("/HTML", new TextResponse("", StartUp.AddFormDataAction)))
-        .Start();
+        .MapPost("/HTML", new TextResponse("", StartUp.AddFormDataAction))
+        .MapGet("/Content", new HtmlResponse(StartUp.DownloadForm))
+        .MapPost("/Content", new TextFileResponse(StartUp.FileName)));
+
+        await server.Start();
+    }
+
+    private static async Task DownloadSitesAsTextFile(string fileName, string[] urls)
+    {
+        var downloads = new List<Task<string>>();
+
+        foreach (var url in urls)
+        {
+            downloads.Add(DownloadWebSiteContent(url));
+        }
+
+        var response = await Task.WhenAll(downloads);
+
+        var responseString = string.Join(Environment.NewLine + new String('-', 100), response);
+
+        await File.WriteAllTextAsync(fileName, responseString);
+    }
+
+    private static async Task<string> DownloadWebSiteContent(string url)
+    {
+        var httpCliet = new HttpClient();
+        using (httpCliet)
+        {
+            var response = await httpCliet.GetAsync(url);
+
+            var html = await response.Content.ReadAsStringAsync();
+
+            return html.Substring(0, 2000);
+        }
+    }
 
     private static void AddFormDataAction(Request request, Response response)
     {
