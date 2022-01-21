9a46a7f359618e6ae18d56cd7ca740ed786fd37d
Ivan Berov <ivanberov3@gmail.com>
2022-01-21 14:14:34 +0100

    add cookie collection

---
+++ CSharpWebBasics/SoftUniHttpServer/SoftUniHttpServer/HTTP/CookieCollection.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+
+namespace SoftUniHttpServer.HTTP
+{
+    public class CookieCollection : IEnumerable<Cookie>
+    {
+        private readonly Dictionary<string, Cookie> cookies;
+
+        public IEnumerator<Cookie> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
