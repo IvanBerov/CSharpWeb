0647371d41265c30c1369ca6a29b0dd7a52609ce
Ivan Berov <ivanberov3@gmail.com>
2022-01-21 14:08:21 +0100

    add cookie

---
+++ CSharpWebBasics/SoftUniHttpServer/SoftUniHttpServer/HTTP/Cookie.cs
@@ -0,0 +1,22 @@
+﻿using SoftUniHttpServer.Common;
+
+namespace SoftUniHttpServer.HTTP
+{
+    public class Cookie
+    {
+        public Cookie(string name, string value)
+        {
+            Guard.AgainstNull(name, nameof(name));
+            Guard.AgainstNull(value, nameof(value));
+
+            Name = name;
+            Value = value;
+        }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+
+        public override string ToString() => $"{this.Name}={this.Value}";
+    }
+}
